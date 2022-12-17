0d0d94037a5d32f5e7f6db7e20a7d1ba39846e1a
jamextech1 <okorojameschizaram@gmail.com>
2022-12-18 00:40:58 +0100

    ci/cd

---
+++ app.js
@@ -0,0 +1,13 @@
+/*==============*/
+/*TYPED JS CODE*/
+/*============*/
+let typed = new Typed(".content-paragraph", {
+  strings: [
+    "",
+    "May This Christmas be filled with, joy and happiness in your life",
+    `You'll live to see the end of 2022`,
+  ],
+  backSpeed: "8",
+  typeSpeed: "9",
+  loop: true,
+});
